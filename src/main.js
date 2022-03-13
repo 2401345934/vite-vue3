ad581cb48c73779c566152cee8f43a7f521e026f
xiaominghui <2401345934@qq.com>
2022-03-13 17:19:45 +0800

    修改tsconfig

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 import { createApp } from 'vue'
 import router from './router/index'
-import App from './App.vue'
+import App from '@/App.vue'
 import store from "@/store/index"
 import 'element-plus/dist/index.css'
 import "@/default.css"
