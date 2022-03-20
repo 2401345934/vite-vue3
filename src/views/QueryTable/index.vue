8bc19ce4ffa6e8e04a44de4b8846736145e3674b
xiaominghui <2401345934@qq.com>
2022-03-20 20:04:08 +0800

    修复查询列表

--- src/views/QueryTable/index.vue
+++ src/views/QueryTable/index.vue
@@ -1,11 +1,12 @@
 
 <template name="queryTable">
+  <!-- search -->
   <div class="search_warp">
     <el-form :inline="true"
              ref="formRef"
              :model="formInline"
              class="form">
-      <div class="actions_warp">
+      <div class="search_from_warp">
         <el-row>
           <el-col v-for="(field,index) in state.fields"
                   :key="index"
@@ -57,9 +58,8 @@
 
             </el-form-item>
           </el-col>
-
         </el-row>
-        <div class="actions">
+        <div class="search_actions">
           <el-form-item>
             <el-button type="primary"
                        @click="onSubmit()">查询</el-button>
@@ -69,47 +69,68 @@
       </div>
     </el-form>
   </div>
-  <el-table :data="state.data"
-            v-loading="loading"
-            class="table_content"
-            :height="height"
-            row-key="id"
-            :highlightCurrentRow='true'
-            empty-text="暂无数据"
-            border>
-    <template v-for="(item,index) in state.columns"
-              :key="index">
-      <el-table-column :prop=" item.dataIndex ||item.key  "
-                       :label="item.title"
-                       :fixed="item.fixed">
-        <template v-if="item.render"
-                  #default="scope">
-          {{item.render(scope.row,scope.$index,scope)}}
-        </template>
-        <template v-if="item.isOperator"
-                  #default="scope">
-          <el-button :type=" 'text' "
-                     :size=" 'small'"
-                     v-for="(btn,i) in item.render"
-                     :key="i"
-                     @click.prevent="btn.action(scope.row,scope)">
-            {{btn.children}}
-          </el-button>
-        </template>
-      </el-table-column>
-    </template>
 
-  </el-table>
-  <el-pagination class="page"
-                 background
-                 v-model:currentPage="pages.currentPage"
-                 v-model:page-size="pages.pageSize"
-                 @size-change="sizeChange"
-                 :page-sizes="[10, 20, 30, 40, 50, 100,200,300,400,500]"
-                 :hide-on-single-page="false"
-                 @current-change="currentChange"
-                 layout="total, sizes, prev, pager, next, jumper"
-                 :total="pages.total" />
+  <!-- actions -->
+  <div class="actions_warp">
+    <el-space>
+      <el-button v-for="(btn,index) in state.actions || []"
+                 :key="index"
+                 @click="btn.action(selectProps,onSubmit)"
+                 :disabled="btn.isDisabled && selectProps && selectProps.length === 0"
+                 :type="btn.type">{{btn.text}}</el-button>
+    </el-space>
+  </div>
+  <!-- table -->
+  <div class="table_warp">
+    <el-table :data="state.data"
+              v-loading="loading"
+              class="table_content"
+              :height="height"
+              row-key="id"
+              ref="queryTableRef"
+              :highlightCurrentRow='true'
+              @selection-change="handleSelectionChange"
+              empty-text="暂无数据"
+              border>
+      <el-table-column :type="state.type"
+                       :v-if="state.type"
+                       width="40" />
+      <template v-for="(item,index) in state.columns"
+                :key="index">
+
+        <el-table-column :prop=" item.dataIndex ||item.key  "
+                         :label="item.title"
+                         :fixed="item.fixed">
+          <template v-if="item.render"
+                    #default="scope">
+            {{item.render(scope.row,scope.$index,scope)}}
+          </template>
+          <template v-if="item.isOperator"
+                    #default="scope">
+            <el-button :type=" 'text' "
+                       :size=" 'small'"
+                       v-for="(btn,i) in item.render"
+                       :key="i"
+                       @click.prevent="btn.action(scope.row,scope)">
+              {{btn.children}}
+            </el-button>
+          </template>
+        </el-table-column>
+      </template>
+
+    </el-table>
+    <el-pagination class="page"
+                   background
+                   v-model:currentPage="pages.currentPage"
+                   v-model:page-size="pages.pageSize"
+                   @size-change="sizeChange"
+                   :page-sizes="[10, 20, 30, 40, 50, 100,200,300,400,500]"
+                   :hide-on-single-page="false"
+                   @current-change="currentChange"
+                   layout="total, sizes, prev, pager, next, jumper"
+                   :total="pages.total" />
+  </div>
+
 </template>
 <script lang="ts" setup>
 // @ts-nocheck
@@ -118,19 +139,29 @@ import type { FormInstance } from "element-plus";
 import { queryParams } from "@/utils/utils";
 import { cloneDeep } from "loadsh";
 import qs from "qs";
-import { he } from "element-plus/lib/locale";
 const formRef = ref<FormInstance>();
 onMounted(() => {
   onSubmit();
 });
-
+//  table ref
+const queryTableRef = ref("");
+// form 表单 属性值
+const formInline = reactive({});
+//  loading
 const loading = ref(false);
+//  分页参数
 const pages = reactive({
   total: 0,
   currentPage: 1,
   pageSize: 10,
 });
-
+// 多选数据
+const selectProps = ref<T>([]);
+// 选中事件
+const handleSelectionChange = (val: T[]) => {
+  selectProps.value = val;
+};
+// 查询事件
 const searchTable = (reset?: boolean) => {
   const value = cloneDeep(formInline);
   const initParams = {
@@ -165,16 +196,14 @@ const searchTable = (reset?: boolean) => {
     },
   });
 };
-// 查询
+// form 表单查询
 const onSubmit = (flag?: boolean) => {
   formRef.value.validate().then(() => {
     pages.currentPage = 1;
     searchTable(flag);
   });
 };
-// 重置
-const formInline = reactive({});
-
+// form 表单 重置
 const resetForm = (formEl: FormInstance | undefined) => {
   if (!formEl) return;
   formEl.resetFields();
@@ -198,6 +227,7 @@ const currentChange = (page: number) => {
 
 const state: any = reactive({
   data: [],
+  type: "selection",
   columns: [
     {
       key: "recordCode",
@@ -260,6 +290,19 @@ const state: any = reactive({
           },
         },
       ],
+    },
+  ],
+  actions: [
+    {
+      // 是否开启勾选启用
+      // isDisabled: true,
+      type: "primary",
+      text: "批量操作",
+      // 勾选的数据
+      // searchtable  刷新表格的方法
+      action: (rows: T[], searchTable: () => void) => {
+        searchTable();
+      },
     },
   ],
   fields: [
@@ -356,21 +399,24 @@ const getTableHeight = () => {
   height.value =
     document.body.clientHeight -
     (document.querySelector(".search_warp")?.clientHeight || 0) -
+    (document.querySelector(".actions_warp")?.clientHeight || 0) -
     200 +
     "px";
 };
 </script>
 <style lang="less">
-.table_content {
-  overflow: auto;
-  width: 100%;
-}
+.table_warp {
+  .table_content {
+    overflow: auto;
+    width: 100%;
+  }
 
-.page {
-  display: flex;
-  margin-top: 8px;
-  margin-right: 8px;
-  justify-content: end;
+  .page {
+    display: flex;
+    margin-top: 8px;
+    margin-right: 8px;
+    justify-content: end;
+  }
 }
 
 .form {
@@ -381,9 +427,13 @@ const getTableHeight = () => {
 }
 
 .actions_warp {
+  margin-bottom: 10px;
+}
+
+.search_from_warp {
   display: flex;
 }
-.actions {
+.search_actions {
   min-width: 200px;
 }
 </style>
