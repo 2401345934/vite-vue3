69b6a8459692b8bcfe806027e5e81288e152d7b8
xiaominghui <2401345934@qq.com>
2022-03-26 22:32:48 +0800

    动画

--- src/views/Test/index.vue
+++ src/views/Test/index.vue
@@ -1,32 +1,16 @@
 <template>
-  <transition name="xmh">
+  <transition
+    appear
+    mode="out-in"
+    enter-active-class="animate__animated animate__lightSpeedInRight"
+    leave-active-class="animate__animated animate__lightSpeedOutRight"
+  >
     <test-components v-show="flag">肖明辉</test-components>
-  </transition>
-  <transition name="xmh2">
-    <test-components v-show="flag">小垃圾</test-components>
   </transition>
   <el-button @click="flag = !flag">切换</el-button>
 </template>
 <script setup lang='ts'>
-import TestComponents from "./components/TestComponents.vue";
+import TestComponents from "./TestComponents.vue";
+
 const flag = ref(false);
 </script>
-<style scoped lang='less'>
-.xmh-enter-from,
-.xmh2-enter-from,
-.xmh-leave-to,
-.xmh2-leave-to {
-  opacity: 0;
-}
-.xmh-enter-active,
-.xmh-leave-active,
-.xmh2-enter-active,
-.xmh2-leave-active {
-  transition: 2s ease;
-}
-
-.v-enter-to,
-.v-leave-from {
-  opacity: 1;
-}
-</style>
