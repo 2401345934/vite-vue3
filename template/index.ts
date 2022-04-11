c7e9f83463d82516b17b7e7a5456072ef9c24d57
alan.xiao <alan.xiao@d1m.cn>
2022-04-11 22:19:51 +0800

    函数拆分

--- template/index.ts
+++ template/index.ts
@@ -1,8 +1,20 @@
+// @ts-nocheck
 const FS = require("fs")
 const inquirer = require("inquirer")
 const VueTemplate = require("./VueTemplate/index.ts")
 const ui = new inquirer.ui.BottomBar();
 
+// 创建成功提示
+const successLog = () => {
+  console.log('success ----------------------------------------------------------------------------------');
+}
+
+// 创建失败提示
+const errorLog = (error) => {
+  console.log(`error -------------------------------------------------------------------${error || '该命名文件已存在'}`);
+}
+
+// 默认执行函数
 const create = () => {
   inquirer.prompt(
     [
@@ -43,47 +55,69 @@ const create = () => {
         type: 'confirm',
         name: 'axios',
         message: '是否需要初始化请求',
-        when:(answers)=>{
+        when: (answers) => {
           return answers.type !== 'QueryTable'
         }
       }
     ]).then((options) => {
-      console.log('您选择的配置项为', options);
       createTemplate(options)
     }).catch((error) => {
-      console.log(`创建失败 -------------------------------------------------------------------${error}`,);
+      errorLog(error)
     });
-
 }
 
+
+// 创建模版处理函数
 const createTemplate = (options) => {
   const { template_name, type, langType, cssLoader, axios } = options;
-  FS.readFile(`src/views/${template_name}/index.vue`, function(err, data) {
-    if(data) {
-      console.log(`创建失败 -------------------------------------------------------------------该命名文件已存在`,);
-    }else{
-      const addConfig = {
-        cssLoader,
-        langType,
-        axios
-      }
+  FS.readFile(`src/views/${template_name}/index.vue`, function (err, data) {
+    if (data) {
+      errorLog()
+      inquirer.prompt(
+        [
+          {
+            type: 'confirm',
+            name: 'iscover',
+            message: '是否要覆盖该文件夹',
+          }
+        ]).then(({ iscover }) => {
+          if (iscover) {
+            writeFs(options, 'writeFile')
+            successLog()
+          } else {
+            errorLog('请重新创建')
+          }
+        }).catch((error) => {
+          errorLog(error)
+        });
+    } else {
       FS.mkdirSync(`src/views/${template_name}`)
-      switch (type) {
-        case "vue2":
-          FS.writeFileSync(`src/views/${template_name}/index.vue`, VueTemplate.createVueTemplate(addConfig))
-          break;
-        case "QueryTable":
-          FS.writeFileSync(`src/views/${template_name}/index.vue`, VueTemplate.createQueryTable(addConfig))
-          break;
-        default:
-          FS.writeFileSync(`src/views/${template_name}/index.vue`, VueTemplate.createVueSetupTemplate(addConfig))
-      }
-      console.log('创建成功 --------------------------------------------------------------------------------------------------------------------------',);
+      writeFs(options, 'writeFileSync')
+      successLog()
     }
   })
-
 }
 
+
+// 创建 template 函数
+const writeFs = (options, writeFileType) => {
+  const { template_name, type, langType, cssLoader, axios } = options;
+  const addConfig = {
+    cssLoader,
+    langType,
+    axios
+  }
+  switch (type) {
+    case "vue2":
+      FS[writeFileType](`src/views/${template_name}/index.vue`, VueTemplate.createVueTemplate(addConfig), () => { })
+      break;
+    case "QueryTable":
+      FS[writeFileType](`src/views/${template_name}/index.vue`, VueTemplate.createQueryTable(addConfig), () => { })
+      break;
+    default:
+      FS[writeFileType](`src/views/${template_name}/index.vue`, VueTemplate.createVueSetupTemplate(addConfig), () => { })
+  }
+}
 
 
 
